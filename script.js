0b2adf913c07c7b7650cf0fe07732500f1fa0238
aditi99r <aditi.ray@student.amity.edu>
2022-07-19 00:56:26 +0530

    expense tracker

--- script.js
+++ script.js
@@ -1,167 +1,42 @@
-@import url('https://fonts.googleapis.com/css?family=Lato&display=swap');
-
-:root {
-  --box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+function addTransaction()
+{
+    var costing = 
+    {
+        id: localStorage.length,
+        name: document.getElementById("text").value,
+        amount: document.getElementById("amount").value
+    };
+    if(costing.name && costing.amount)
+    {
+        localStorage.setItem(costing.id, JSON.stringify(costing));
+    }
 }
 
-* {
-  box-sizing: border-box;
-}
+var positive = 0.00, negative = 0.00, balance = 0.00;
 
-body {
-  background-color: #be2edd;
-  background-size: cover;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  min-height: 100vh;
-  margin: 0;
-  font-family: 'Lato', sans-serif;
+for (let i = localStorage.length ; i>0; i--) 
+{
+    var item = JSON.parse(localStorage.getItem(i-1));
+    var li = document.createElement("li");
+    if(item.amount<0)
+    {
+        li.classList.add("minus");
+        negative = negative + parseFloat(item.amount);
+        negative.toFixed(2);
+    }
+    else 
+    {
+        li.classList.add("plus");
+        positive = positive + parseFloat(item.amount);
+    }
+    var ul = document.getElementById("list");
+    ul.appendChild(li);
+    li.innerHTML = "<div>"+item.name+"</div> <div>"+item.amount+"</div>" ;
 }
-
-.container {
-  margin: 20px auto;
-  width: 350px;
-  border:1px solid black;
-  padding: 2%;
-  background: #f5f7f6;
- 
-}
-
-h1 {
-  letter-spacing: 1px;
-  margin: 0;
-}
-
-h3 {
-  border-bottom: 1px solid #bbb;
-  padding-bottom: 10px;
-  margin: 40px 0 10px;
-  width: 100%;
-}
-.histroy{
-  position: absolute;
-  margin-top:-8% ;
-  margin-left:30% ;
-  width: 15%;
-  height: 10%;
-
-
-}
-
-h4 {
-  margin: 0;
-  text-transform: uppercase;
-}
-
-.inc-exp-container {
-  background-color: #fff;
-  box-shadow: var(--box-shadow);
-  padding: 20px;
-  display: flex;
-  justify-content: space-between;
-  margin: 20px 0;
-}
-
-.inc-exp-container > div {
-  flex: 1;
-  text-align: center;
-}
-
-.inc-exp-container > div:first-of-type {
-  border-right: 1px solid #dedede;
-}
-
-.money {
-  font-size: 20px;
-  letter-spacing: 1px;
-  margin: 5px 0;
-}
-
-.money.plus {
-  color: #2ecc71;
-}
-
-.money.minus {
-  color: #c0392b;
-}
-
-label {
-  display: inline-block;
-  margin: 10px 0;
-}
-
-input[type='text'],
-input[type='number'] {
-  border: 1px solid #dedede;
-  border-radius: 2px;
-  display: block;
-  font-size: 16px;
-  padding: 10px;
-  width: 100%;
-}
-
-.btn {
-  cursor: pointer;
-  background-color: #9c88ff;
-  box-shadow: var(--box-shadow);
-  color: #fff;
-  border: 0;
-  display: block;
-  font-size: 16px;
-  margin: 10px 0 30px;
-  padding: 10px;
-  width: 100%;
-}
-
-.btn:focus,
-.delete-btn:focus {
-  outline: 0;
-}
-
-.list {
-  list-style-type: none;
-  padding: 0;
-  margin-bottom: 40px;
-}
-
-.list li {
-  background-color: #fff;
-  box-shadow: var(--box-shadow);
-  color: #333;
-  display: flex;
-  width: 150%;
-  justify-content: space-between;
-  position: relative;
-  padding: 10px;
-  margin: 10px 0;
-}
-
-.list li.plus {
-  border-right: 5px solid #2ecc71;
-}
-
-.list li.minus {
-  border-right: 5px solid #c0392b;
-}
-
-.delete-btn {
-  cursor: pointer;
-  background-color: #e74c3c;
-  border: 0;
-  color: #fff;
-  font-size: 20px;
-  line-height: 20px;
-  padding: 2px 5px;
-  position: absolute;
-  top: 50%;
-  left: 0;
-  transform: translate(-100%, -50%);
-  opacity: 0;
-  transition: opacity 0.3s ease;
-}
-
-.list li:hover .delete-btn {
-  opacity: 1;
-}
+balance = negative + positive;
+negative = negative.toFixed(2);
+positive = positive.toFixed(2);
+balance = balance.toFixed(2);
+document.getElementById("balance").innerHTML = "$"+balance;
+document.getElementById("money-plus").innerHTML = "+$"+positive;
+document.getElementById("money-minus").innerHTML = "-$"+negative;
